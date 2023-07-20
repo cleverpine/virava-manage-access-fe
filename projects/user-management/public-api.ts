c35d1bc613ddea827557aff45d07a7ffa60af8b3
Ivaylo Garnev <ivaylo.garnev@cleverpine.com>
2023-07-20 12:57:52 +0300

    removed uneseccesary files

--- projects/user-management/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of user-management
- */
-
-export * from './src/lib/user-management-lib.module';
-export * from './src/lib/components/users-listing/users-listing.component';
-export * from './src/lib/openapi/api-configuration';
-export * from './src/lib/services/error.service';
